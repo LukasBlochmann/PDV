3cfef049631663915d9b68a5b43ef986a51626bd
Gerrit Erdt <gerrit.erdt@gmail.com>
2024-04-24 16:13:35 +0200

    header finally fixed

--- 03/main/pdv_03_lib.h
+++ 03/main/pdv_03_lib.h
@@ -5,13 +5,10 @@ class Motor {
   public:
     Motor(int number_of_steps, int pin_1, int pin_2, int pin_3, int pin_4);
     
-    // speed setting
     void speed(long what_speed);
 
-    // calculate delay and number of steps
     void step(int steps_to_move);
 
-    // align the motor to the initial position
     void calibrate();
 
     void check_buttons();
@@ -32,52 +29,33 @@ class Motor {
     int up_pin;
     int down_pin;
 
-    int running_dir;
-
     bool up_pin_check = false;
     bool down_pin_check = false;
     bool deadman_pin_check = false;
     unsigned long last_step_time; 
     unsigned long step_delay; 
-
+    int running_dir; // Indicates the current direction of rotation
 
   private:
-    // execute calibrated steps
     void execute_steps(int this_step);
 
-    // user interface for calibration
     void print_initial_help();
 
-    // upin = user pin, given by master
     int upin_1;
     int upin_2;
     int upin_3;
     int upin_4;
-
-    // needed in calibration func as an cancel indicator
-    bool is_any_button_pressed;
-
-    // used in calibration func, declares the buttons used for following actions
-    int forward;
-    int backwards;
     int confirmation_pin;
 
-    // used in calibration func, to prevent bouncing
-    int calibration_button_last;
+    bool is_any_button_pressed;
 
-    // following variables are used for direct interaction with the motor
-    int direction;          
-    int number_of_steps;      
-    int pin_count;            
-    int step_number;
- 
-    // variables needed for normal prosedure (deadman switch)
-    int deadman_on;
-    int exit_pos;
-    bool set_back;
+    int calibration_button_last;
+    
     bool calibrating;
-    bool is_returning;
+
+    int direction;          
+    int number_of_steps;         
+    int step_number;
 };
 
 #endif
-
