b4feb4e37860268ad6e7428058be4bac54f82eba
Gerrit Erdt <gerrit.erdt@gmail.com>
2024-04-24 14:07:45 +0200

    Fixes: - Code restructuring - comments - simple header remake
    ToDo:
    - Check if all header defined variables are correct and necessary
    - Verify if holding the confirm button pressed during calibration immediately proceeds because of too little delay

--- 03/main/main.ino
+++ 03/main/main.ino
@@ -1,59 +1,54 @@
 #include "pdv_03_lib.h"
 
-
-
-// RED: 3, GREEN: 4,, YELLOW: 5, BLUE: 6
-
-// 1: 0 1 0 1
-// 2: 0 1 1 0
-// 3: 1 0 1 0
-// 4: 1 0 0 1
-
+// Names derived from cable colors
 const int RED = 4, GREEN = 5, YELLOW = 6, BLUE = 7;
 int steps_to_go = 50;
 
-
+//Create motor based on the pdv_03_lib.cpp file
 Motor motor = Motor(200, BLUE, YELLOW, GREEN, RED);
 
 void setup(){
   Serial.begin(9600);
+  //Set speed to 30 rpm
   motor.speed(30);
 }
 
 void loop(){
-
-
+  // Time initialization
   motor.now = millis();
+  // Calibrate the motor if it is not yet calibrated or needs re-calibration and the re-calibration buttons are pressed for 3 seconds
   if(motor.calibrated == false || ((motor.now - motor.last_time >= 3000) && motor.calibrate_repeat())){
-    
+    // Necessary for initial startup because of not-yet set variables
     if(motor.calibrated == false){
       motor.calibrate();
     }
-
   }
 
   motor.check_buttons();
 
+  // Only perform up/down motor movement if the deadman-switch is pressed
   if(motor.deadman_pin_check == true){
+    // If the motor is either already going up or
+    // The down button is not pressed, it is not in the up position, the up button is pressed and it was in down position
     if(motor.down_pin_check == false && motor.standard_position != 50
-      && (((motor.up_pin_check == true) && motor.standard_position == 0) || motor.running_dir == 1)){          
+      && (((motor.up_pin_check == true) && motor.standard_position == 0) || motor.running_dir == 1)){        
+      // Perform one stop upwards, adjust the current position and set the direction to upwards  
       motor.step(1);
       motor.standard_position += 1;
       motor.running_dir = 1;
-    }else if(motor.standard_position != 0 && (motor.down_pin_check == true || motor.running_dir == 0)){
+    }
+    // If the motor is not in down position, and down is pressed OR it is already moving downwards
+    // The last condition lets the down button overwrite all other movements
+    else if(motor.standard_position != 0 && (motor.down_pin_check == true || motor.running_dir == 0)){
+      // Perform one step downwards, adjust the current position and set the direction to downwards
       motor.step(-1);
       motor.standard_position -= 1;
       motor.running_dir = 0;
     }
-  }else if(motor.standard_position != 50 && motor.standard_position != 0){
+  }
+  // If the deadman-switch is not pressed and the motor is neither in op nor in down position, move it downwards
+  else if(motor.standard_position != 50 && motor.standard_position != 0){
     motor.step(-1);
     motor.standard_position -= 1;
   }
-
-
- 
-
-
-
-
 }
--- 03/main/pdv_03_lib.cpp
+++ 03/main/pdv_03_lib.cpp
@@ -6,19 +6,16 @@
 Motor::Motor(int number_of_steps, int pin_1, int pin_2, int pin_3, int pin_4){
   // Initialize variables for internal motor control
   this->step_number = 0;    
-  this->direction = 0;      
-  this->last_step_time = 0; 
-  this->number_of_steps = number_of_steps; 
+  this->direction = 0; //Direciton, 0 for down, 1 for up  
+  this->last_step_time = 0; //Time of the last performed step
+  this->number_of_steps = number_of_steps; //Number of steps for one revolution
   this->calibrated = false; //If the motor is currently calibrated
   this->calibration_button_last = 1; 
   this->is_any_button_pressed = false;
   this->standard_position; //Position the motor is in after a successful movement
-  this->exit_pos; //Absolute value of the remaining steps
-  this->calibrating = false;
-  this->running_dir = 0; //Direction the motor is currently moving in
-
-  this->now = 0;
-  this->last_time = 0;
+  this->calibrating = false; //If the motor is currently being calibrated
+  this->now = 0; //Internal time tracking variable
+  this->last_time = 0; //
 
   // Pins that are connected to the motor
   this->upin_1 = pin_1;
@@ -31,9 +28,11 @@ Motor::Motor(int number_of_steps, int pin_1, int pin_2, int pin_3, int pin_4){
   this->down_pin = 11;
   this->confirmation_pin = 12;
   this->deadman_pin = 13;
-  this->up_pin_check = false;
-  this->down_pin_check = false;
-  this->deadman_pin_check = false;
+
+  // Button states
+  this->up_pin_check = false;  //True in case up is pressed
+  this->down_pin_check = false; //True in case down is pressed
+  this->deadman_pin_check = false; //True in case deadman-switch is pressed
   
   // Configure the user communication pins as inputs
   for (int i = 10; i <= 13; i++){pinMode(i, INPUT);}
@@ -46,50 +45,50 @@ Motor::Motor(int number_of_steps, int pin_1, int pin_2, int pin_3, int pin_4){
 }
 
 // Set the speed in revs per minute
-void Motor::speed(long whatSpeed){
-  this->step_delay = 60L * 1000L * 1000L / this->number_of_steps / whatSpeed; //! ?????????????????????????????????????????????
+void Motor::speed(long what_speed){
+  this->step_delay = 60L * 1000L * 1000L / this->number_of_steps / what_speed; //! ?????????????????????????????????????????????
+  // Use as longs because of the data type of the micros() function
+  // 60 Minutes * 1000 ms * 1000 µs / number of steps / speed in rpm = delay between steps in µs
 }
 
-// Perform a given number of steps
-void Motor::step(int steps_to_move){ // !!!!!!
-  //steps_to_move: Number of steps to move, positive for up and negative for down
-
+// Perform a given number of steps in a given direction, normally 1 or -1
+void Motor::step(int steps_to_move){ 
   int steps_left = abs(steps_to_move);  
 
   // determine direction based on whether steps_to_move is + or -:
   if (steps_to_move > 0) { this->direction = 1; }
   else if (steps_to_move < 0) { this->direction = 0; }
 
-  //! decrement the remaining steps
   while (steps_left > 0){
-
     // only move if interval has passed
     unsigned long now = micros();
     if (now - this->last_step_time >= this->step_delay){
-      // timestamp for next iteration
+      // Set timestamp for next step
       this->last_step_time = now;
       
       // increment or decrement step_number based on direction
       if (this->direction == 1){
         this->step_number++;
+        // If the current step number is equal to the number of steps in one revolution, reset it to 0 to start over again
         if (this->step_number == this->number_of_steps) {this->step_number = 0;}
       }else{
+        // If the current step number is equal to 0, set it to the number of steps in one revolution to start over again
         if (this->step_number == 0) {this->step_number = this->number_of_steps;} //number of steps = 200
         this->step_number--;
       }
-      // c.f. l. 53
+      
+      // Decrement steps
       steps_left--;
-      // modulo 4 to choose the current phase, which also determines the direction
+      // modulo 4 to choose the current phase, which also determines the direction used internally
       execute_steps(this->step_number % 4);
     }
   }
-  // If all steps were performed, return 0
 }
 
 // Physical communication with the motor
-void Motor::execute_steps(int thisStep){
+void Motor::execute_steps(int this_step){
   // depending in which phase we are currently in, the function passes the sequence
-  switch (thisStep){
+  switch (this_step){
     case 0:  // 1010
       digitalWrite(upin_1, HIGH);
       digitalWrite(upin_2, LOW);
@@ -118,20 +117,27 @@ void Motor::execute_steps(int thisStep){
 }
 
 bool Motor::prevent_bouncing(int target_button, int last_state){
+  unsigned long now = micros();
+  // Intentionally block the processor because in this situation, we have to forcefully stop all other actions
   while(1){
-    unsigned long now = micros();
+    now = micros();
+    // 
     if(now - this->last_step_time >= 200){
       this->last_step_time = now;
+      // This is the only relevant change in state, so we only have to check this. It detects a change from LOW to HIGH
       if((digitalRead(target_button) == HIGH) && last_state == 0){
+        // Exit the function and return success
         return true;
       }
     }   
+    // Get the last state
     last_state = (digitalRead(target_button) == LOW) ? 0 : last_state;
   }     
 }
 
 // Initial communication with user before motor is used for the first time
 void Motor::print_initial_help(){
+  // Print calibration menu
   Serial.println("\nYou have to calibrate the motor before using it!\n");
   Serial.println("The motor is correctly calibrated, when the cabinet is positioned horizontally, to the left of the motor, when looking at it from the cabinet side.\n");
   Serial.println("Please press the Buttons accordingly (left to right): ");
@@ -143,11 +149,12 @@ void Motor::print_initial_help(){
 
 // Calibrate the motor
 void Motor::calibrate(){
+  // Print how to calibrate the motor
   print_initial_help();
   
   //Indicate the motor is being calibrated
   this->calibrating = true;
-  // calibration loop
+  // Intentionally block the processor because in this situation, we have to forcefully stop all other actions before calibration is done
   while(this->calibrated != true){
     // Check which button is pressed, handle accordingly
     if ((digitalRead(this->up_pin)) == HIGH){
@@ -158,11 +165,11 @@ void Motor::calibrate(){
       Serial.println("Please check again if the motor is calibrated correctly and confirm again.");
       Serial.println("Press any other button to continue the calibration.\n");
 
-      //Prevent bouncing by checking if the button was released before being pressed again
+      // Setting the last button state as up for later bouncing prevention
       this->calibration_button_last = 1;
       
       while(this->is_any_button_pressed == false){
-        // check if any other button is pressed, cancel calibration
+        // check if any other button is pressed, cancel confirmation
         if((digitalRead(this->up_pin)) == HIGH || digitalRead((this->down_pin)) == HIGH || digitalRead(13) == HIGH){
           Serial.println("Confirmation has been canceled. Proceeding calibration.\n");
           this->is_any_button_pressed = true;
@@ -186,12 +193,16 @@ void Motor::calibrate(){
 }
   
 void Motor::check_buttons(){
+  // Check and save the states of all buttons (low = false, high = true)
   this->up_pin_check = (digitalRead(this->up_pin) == HIGH) ? true : false;
   this->down_pin_check = (digitalRead(this->down_pin) == HIGH) ? true : false;
   this->deadman_pin_check = (digitalRead(this->deadman_pin) == HIGH) ? true : false;
 }
 
 int Motor::calibrate_repeat() {
+  // Repeat calibration if all 4 buttons are pressed at the same time and hold for 3 seconds
+  // We encountered one single test where the motor lost track of its position, however, we could not reproduce this error
+  // In case it ever happens, this provides a solution to re-calibrate the motor
     if (digitalRead(this->confirmation_pin) == HIGH &&
         digitalRead(this->up_pin) == HIGH &&
         digitalRead(this->down_pin) == HIGH &&
@@ -199,13 +210,18 @@ int Motor::calibrate_repeat() {
 
           this->last_time = this->calibrating == true ? this->last_time : millis();
 
+          // Indicate uncalibrated state if it is calibrating
           if(this->calibrating == true){
             this->calibrated = false;
-          }else{
+          }else
+          // Re-Calibration detected
+          {
             Serial.println("Calibrating attempt detected.");
             this->calibrating = true;
           }
+          // User-canceled re-calibration
           return 1; 
         }
+    // Successfuly re-calibrated
     return 0;
 }
